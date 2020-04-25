a1b80a7570fb781209adbb1e69dbb58b593be969
Aminul Huq <41773621+aminul-huq@users.noreply.github.com>
2020-04-25 10:55:43 +0600

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-python train_model.py --pretrain=0\
-                        --attack=4 \
-                        --lr=0.001\
-                        --itr=76 \
-                        --momentum=0.9\
-                        --eps=0.03137\
-                        --step_size=20\
-                        --overshoot=0.2\
-                        --preprocess=0
